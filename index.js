26b99022ed3bf9baa32c52ad7baa0c79142d2d08
Eligio93 <eligio.cristantielli@gmail.com>
2023-11-21 16:09:26 +1100

    adds contains function

--- index.js
+++ index.js
@@ -1,94 +1,116 @@
-class LinkedList{
+class LinkedList {
 
-  constructor(){
-    this.list=null;
-    this.listSize=0;
+  constructor() {
+    this.list = null;
+    this.listSize = 0;
   }
 
-  append(value){
-    if(this.list==null){
-      this.list=new Node(value);
-    }else {
-      let current=this.list
-      while(current.nextNode!== null){
-        current=current.nextNode;
+  append(value) {
+    if (this.list == null) {
+      this.list = new Node(value);
+    } else {
+      let current = this.list
+      while (current.nextNode !== null) {
+        current = current.nextNode;
       }
-      current.nextNode=new Node(value)
-  }
-  this.listSize+=1;   
-  }
-  prepend(value){
-    if(this.list==null){
-      this.list=new Node(value);
-    }else{
-      this.list=new Node(value,this.list)
+      current.nextNode = new Node(value)
     }
-    this.listSize+=1;
+    this.listSize += 1;
   }
-  size(){
+  prepend(value) {
+    if (this.list == null) {
+      this.list = new Node(value);
+    } else {
+      this.list = new Node(value, this.list)
+    }
+    this.listSize += 1;
+  }
+  size() {
     return this.listSize;
   }
-  head(){
+  head() {
     return this.list
   }
-  tail(){
-    let current=this.list;
-    while(current.nextNode!== null){
-      current=current.nextNode
+  tail() {
+    let current = this.list;
+    while (current.nextNode !== null) {
+      current = current.nextNode
     }
     return current
   }
-  at(index){
-    if(this.list == null){
+  at(index) {
+    if (this.list == null) {
       return "No elements in the list"
-    }else if(index==0){
+    } else if (index == 0) {
       return this.list
-    }else if(index > this.listSize-1){
+    } else if (index > this.listSize - 1) {
       return "No elements at that index"
-    }else{
-      let cont=0;
-      let current=this.list;
-      while(cont<index){        
-        current=current.nextNode;
+    } else {
+      let cont = 0;
+      let current = this.list;
+      while (cont < index) {
+        current = current.nextNode;
         cont++;
       }
       return current
     }
   }
-  pop(){
-    if(this.list==null){
+  pop() {
+    if (this.list == null) {
       return "The list is empty"
-    }else{
-      let current=this.list;
-      let prev=current;
-      while(current.nextNode !== null){ 
-        prev=current;      
-        current=current.nextNode
+    } else {
+      let current = this.list;
+      let prev = current;
+      while (current.nextNode !== null) {
+        prev = current;
+        current = current.nextNode
       }
-      prev.nextNode=null;
-      this.listSize-=1;
+      prev.nextNode = null;
+      this.listSize -= 1;
+    }
+  }
+  contains(value) {
+    if (this.list == null) {
+      return "The list is empty"
+    } else {
+      let current = this.list;
+      while (current.nextNode !== null) {
+        if (current.value == value) {
+          return true;
+          break;
+        } else {
+          current = current.nextNode;
+        }
+      }
+      if (current.value == value) {
+        return true
+      } else {
+        return false
+      }
+
     }
   }
 
 }
-class Node{
-  constructor(value=null,nextNode=null){
-    this.value=value,
-    this.nextNode=nextNode;
+class Node {
+  constructor(value = null, nextNode = null) {
+    this.value = value,
+      this.nextNode = nextNode;
 
   }
 }
 // LinkedList.append(3);
-let linkedList= new LinkedList();
+let linkedList = new LinkedList();
 linkedList.append(2);
 linkedList.append(3);
 linkedList.append(4);
 linkedList.prepend(1);
 linkedList.append(5);
 linkedList.pop();
+linkedList.contains(2);
 
 
-console.log(linkedList.list);
+console.log(linkedList.contains(4));
 
 
 
